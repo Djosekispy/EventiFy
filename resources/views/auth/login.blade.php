3201394c757e3eefa0ac7e08b5e1bb1d19afbed9
Djosekispy <globof129@mail.com>
2025-02-26 11:25:32 +0100

    Message done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,9 +11,7 @@
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul class="list-unstyled mb-0">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
+                   Credenciais incorrectas
                 </ul>
             </div>
             
